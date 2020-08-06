e79fefc588c9a9ae67b26ec00d8ca9b63aa58cc8
于爱龙 <yuailong@100tal.com>
2020-08-06 11:47:16 +0800

    Updating git submodule updating tool ...
    Change-Id: Icd0fca75537c35edcf890a95e9b8cf20519d4c26

--- toolUpdateProjects.sh
+++ toolUpdateProjects.sh
@@ -1,8 +1,6 @@
 #/bin/bash
 
-# This program is used for creating branches of main and its submodules.
-
-echo "Tal branch checkouting program is running now."
+echo "Tal branch checkouting program is running now, the trick is parcing .gitmodules for key information."
 echo
 
 echo "Before run this script, make sure you have clone your submodules properly by calling [git submodule update]"
@@ -12,108 +10,53 @@ currentBranch=$(git symbolic-ref --short HEAD)
 read -p "Current branch is $currentBranch, are you sure to continue? Please ensure your current branch is clean, submodules are set correctly. Press any key to contine, [ctrl + c]  to stop." -n 1 -r
 echo
 
-#echo "Try to set submodule to main submitted hashcode"
-#git submodule update
-
 targetBranch=$currentBranch
 while [ "$targetBranch" == "" ]; do
   read -p "Please input your target branch name: " targetBranch
 done
-
-echo "Your target branch is $targetBranch"
-echo
-
-updateGithubs="y"
-while [ "$updateGithubs" == "" ]; do
-  read -p "Do we update githubs? y or n : " updateGithubs
-done
-
 
 echo "Updating root project ..."
 git fetch
 git checkout $targetBranch
 git pull --rebase
 echo "Root project updated."
+echo "Done"
+echo
 echo
 
-if [ "$updateGithubs" == "y" ]; then
-    PROJECTS="QtComposition qtpromise QtRestClient QtEventBus  log4qt QtJsonSerializer"
-    echo "Updating github projects ($PROJECTS)"
-    echo
-    for g in $PROJECTS
-    do
-        echo "Updating $g ..."
-        cd $g
-        echo "Entering $(pwd)"
-        git fetch
-        git checkout master
-        git pull --rebase
-        cd ..
-        echo
-    done
-fi
-
-if [ "$updateGithubs" == "y" ]; then
-    PROJECTS="thirdparty docCovert"
-    echo "Updating github projects ($PROJECTS)"
-    echo
-    for g in $PROJECTS
-    do
-        echo "Updating $g ..."
-        cd $g
-        echo "Entering $(pwd)"
-        git fetch
-        git checkout develop
-        git pull --rebase
-        cd ..
-        echo
-    done
-fi
-
-PROJECTS="ShowBoard Geometry InkCanvas TeachingTools TeachingLib EventsLog InteractBase InteractionLib Account Main RobotPen UiBase Salt TalWeb Assistant"
-echo "Updating tal projects ($PROJECTS)"
+echo "Starting updating submodules by parcing \".gitmodules\" file"
 echo
-for g in $PROJECTS
+
+TargetPath=""
+TargetBranch=""
+
+cat ./.gitmodules | while read LINE
 do
-    echo "Updating $g ..."
-    cd $g
-    echo "Entering $(pwd)"
-    git fetch
-    git checkout develop/master
-    git pull --rebase
-    cd ..
-    echo
-done
+        if [[ "$LINE" == "[submodule"* ]]; then
+              TargetPath=""
+              TargetBranch=""
+        fi
 
-PROJECTS="Html"
-echo "Updating tal projects ($PROJECTS)"
-echo
-for g in $PROJECTS
-do
-    echo "Updating $g ..."
-    cd $g
-    echo "Entering $(pwd)"
-    git fetch
-    git checkout feature/1.8
-    git pull --rebase
-    cd ..
-    echo
-done
+        if [[ "$LINE" == "path"*"="* ]]; then
+              TargetPath=$(echo "${LINE##*=}" | tr -d " \t\n\r")
+        fi
 
-PROJECTS="XhtHtml"
-echo "Updating tal projects ($PROJECTS)"
-echo
-for g in $PROJECTS
-do
-    echo "Updating $g ..."
-    cd $g
-    echo "Entering $(pwd)"
-    git fetch
-    git checkout feature/815
-    git pull --rebase
-    cd ..
-    echo
-done
+        if [[ "$LINE" == "branch"*"="* ]]; then
+              TargetBranch=$(echo "${LINE##*=}" | tr -d " \t\n\r")
+        fi
 
-echo "Entering $(pwd)"
-echo "All done."
+        if [[ $TargetPath != "" &&  $TargetBranch != "" ]]; then
+              echo "Checking module \"$TargetPath\" of branch \"$TargetBranch\" "
+              cd $TargetPath
+              echo "Entering $(pwd)"
+              git fetch
+              git checkout $TargetBranch
+              git pull --rebase
+              cd ..
+              echo "Entering $(pwd)"
+              echo "Done"
+              echo
+              echo
+        fi
+
+done
